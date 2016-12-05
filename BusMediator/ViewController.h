60c3d56103a57076c87f69f511949a8cece1a54d
TsingZhang <history_zq@163.com>
2016-12-05 17:22:34 +0800

    init

---
+++ BusMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BusMediator
+//
+//  Created by Zhang Qing on 16/12/5.
+//  Copyright © 2016年 Tsing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BusMediator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BusMediator
+//
+//  Created by Zhang Qing on 16/12/5.
+//  Copyright © 2016年 Tsing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
